d8cf3ed40c83db16567c3402d07f4aa975f61cff
votre nom <votre adresse mail>
2025-11-25 12:42:08 +0100

    1. ameliorer room, ajouter champs de enter

--- src/modelisation/Room.java
+++ src/modelisation/Room.java
@@ -1,5 +1,39 @@
 package modelisation;
 
+import java.util.Objects;
+
+public class Room {
+    private RoomType type;
+    private boolean visite;
+    private final Enter enter;
+
+    public Room(RoomType type,Enter e) {
+    	Objects.requireNonNull(e);
+    	Objects.requireNonNull(type);
+        this.type = type;
+        this.visite = false;
+        this.enter=e;
+    }
+
+    public RoomType getType() { 
+    	return type; 
+    }
+
+    public void setType(RoomType t) { 
+    	this.type = t; 
+    }
+
+    public boolean estVisite() { 
+    	return visite; 
+    }
+
+    public void visiter() { 
+    	if(estVisite()) {return;}
+    	this.visite = true; 
+    	enter.apply(null);
+    }
+}
+/*
 public class Room {
     private RoomType type;
     private boolean visite;
@@ -24,4 +58,4 @@ public class Room {
     public void visiter() { 
     	this.visite = true; 
     }
-}
+}*/
